f09171604e84f15d354d6470f2e0dafba8396201
Kirill Kataev <kkataev@gmail.com>
2013-12-13 17:53:39 +0400

    Main slide control

--- presentation.js
+++ presentation.js
@@ -1,5 +1,40 @@
 var Presentation = (function(){
 	function init (options) {
-		console.log (options + ' ' + $(".slide"))
+		$(".slide").each( function(index) {
+			if (index == 0) 
+				$(this).addClass("active")
+			else
+				$(this).addClass("inactive")
+		});
+		document.onkeydown = function(e) {
+    		e = e || window.event;
+    		switch(e.which || e.keyCode) {
+				case 37: 
+					console.log("Get Previous Slide");
+					getPreviousSlide();
+				break;
+				case 39: 
+					console.log("Get Next Slide");
+					getNextSlide();
+				break;
+				default: return;
+			}
+			e.preventDefault();
+		}
+
+	}
+	function getNextSlide() {
+		if ($(".slide.active").next().is("div.slide")) {
+			$(".slide.active").next().removeClass("inactive").addClass("active");
+			$(".slide.active").first().removeClass("active").addClass("inactive");
+		}
+	}	
+	function getPreviousSlide() {
+		$(".slide.active").prev().removeClass("inactive").addClass("active");
+		$(".slide.active").next().removeClass("active").addClass("inactive");
+	}	
+	return {
+		init: init,
+		getNextSlide: getNextSlide
 	}
 })()
